b4b1c0a78fbe61f92b369d87544dbb66717dbbb6
Giovanni <giovannidesilvestri@gmail.com>
2024-05-17 14:46:36 +0200

    annotation @EnableAspectJAutoProxy addedd to AuthenticationFromExternalSecurityServiceApplication.class

--- src/main/java/com/giogio/AuthenticationFromExternalSecurityServiceApplication.java
+++ src/main/java/com/giogio/AuthenticationFromExternalSecurityServiceApplication.java
@@ -2,10 +2,12 @@ package com.giogio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class AuthenticationFromExternalSecurityServiceApplication {
 
